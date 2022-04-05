40d43d6b1fad28b93628dfb31a18a9b6cb750e03
rahul6975 <rahulya7569@gmail.com>
2022-04-05 15:38:55 +0530

    [FEATURE/INTEGRATE-MVVM] | Added mvvm architecture

---
+++ app/src/main/java/com/rahul/kotlinmvvmapp/views/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.rahul.kotlinmvvmapp.views
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.rahul.kotlinmvvmapp.databinding.ActivityMainBinding
+import com.rahul.kotlinmvvmapp.model.models.ResponseClass
+import com.rahul.kotlinmvvmapp.viewAdapter.ViewAdapter
+import com.rahul.kotlinmvvmapp.viewModel.GitViewModel
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var gitViewModel: GitViewModel
+    private lateinit var viewAdapter: ViewAdapter
+    var message = ""
+    private var list = arrayListOf<ResponseClass>()
+    private lateinit var mainActivityBinding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainActivityBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainActivityBinding.root)
+        gitViewModel = ViewModelProvider(this)[GitViewModel::class.java]
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onResume() {
+        setRecyclerView()
+        fetchData()
+        super.onResume()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun fetchData() {
+        if (isOnline(this)) {
+            val result = gitViewModel.fetchResult()
+            result.observe(this, Observer {
+                if (it != null) {
+                    list.addAll(it)
+                    viewAdapter.notifyItemChanged(0)
+                    message =
+                        "List of all closed pull request from repo : ${it[0].head?.repo?.name}"
+                    mainActivityBinding.tvTitle.text = message
+                }
+            })
+            mainActivityBinding.progressCircular.visibility = View.GONE
+        } else {
+            Toast.makeText(this, "Please check your internet", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setRecyclerView() {
+        val linearLayoutManager = LinearLayoutManager(this)
+        viewAdapter = ViewAdapter(list)
+        mainActivityBinding.recyclerView.apply {
+            adapter = viewAdapter
+            layoutManager = linearLayoutManager
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun isOnline(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                return true
+            }
+        }
+        return false
+    }
+}
